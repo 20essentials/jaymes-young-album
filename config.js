65ae1662eddac6020b490f17f22b332daed69958
DominicDev <s0l0estudi4r@gmail.com>
2025-10-25 12:37:19 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'JAYMES YOUNG';
+export let nameSongs = [
+	"INFINITY", /*1*/
+	"I'LL BE GOOD", /*2*/
+	"MOONDUST", /*3*/
+	"HABITS OF MY HEART", /*4*/
+	"BLACK MAGIC", /*5*/
+	"WE WON'T", /*6*/
+	"DON'T YOU KNOW", /*7*/
+	"WHAT IS LOVE", /*8*/
+	"STONED ON YOU", /*9*/
+	"TWO PEOPLE", /*10*/
+	"FEEL SOMETHING", /*11*/
+	"ONE LAST TIME", /*12*/
+	"HAPPIEST YEAR", /*13*/
+	"NORTHERN LIGHTS", /*14*/
+	"HOLD YOU DOWN", /*15*/
+	"SUGAR BURN", /*16*/
+	"PARACHUTE", /*17*/
+	"DON'T YOU KNOW", /*18*/
+	"TIED DOWN", /*19*/
+	"DARK STAR", /*20*/
+]
